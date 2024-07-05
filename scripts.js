e22cba6d48b66231a5f2b86a02b0d8cdfdcadf11
macca1b <brendanmaccabe@gmail.com>
2024-07-05 20:27:23 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,3 +10,11 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
+
+
+const menuToggle = document.getElementById('mobile-menu');
+const nav = document.querySelector('.nav');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('active');
+});
