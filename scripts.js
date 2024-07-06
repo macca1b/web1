975dd926558a400b604168450fd959ec43cfc9b0
macca1b <brendanmaccabe@gmail.com>
2024-07-06 12:32:59 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -10,11 +10,3 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 });
-
-
-const menuToggle = document.getElementById('mobile-menu');
-const nav = document.querySelector('.nav');
-
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
