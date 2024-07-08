2d0f85d3a0ae0c0194d910514adc12c73a98bbd8
macca1b <brendanmaccabe@gmail.com>
2024-07-08 01:31:52 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,12 +1,8 @@
-// scripts.js
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
-        });
-    });
-});
+function toggleMenu() {
+    const navLinks = document.getElementById('nav-links');
+    if (navLinks.style.display === 'block') {
+        navLinks.style.display = 'none';
+    } else {
+        navLinks.style.display = 'block';
+    }
+}
