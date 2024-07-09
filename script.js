bb43a946b15aba1b17d3742e21cf09a2cdda05ed
macca1b <brendanmaccabe@gmail.com>
2024-07-09 18:00:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
 document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM fully loaded and parsed');
+
     function toggleMenu() {
-        const navLinks = document.getElementById('nav-links');
+        const navLinks = document.querySelector('.nav-links');
+        console.log('Toggle menu');
         navLinks.classList.toggle('active');
     }
 
     const menuIcon = document.querySelector('.menu-icon');
-    menuIcon.addEventListener('click', toggleMenu);
+    if (menuIcon) {
+        menuIcon.addEventListener('click', toggleMenu);
+        console.log('Menu icon found and event listener added');
+    } else {
+        console.log('Menu icon not found');
+    }
 
     const navLinkElements = document.querySelectorAll('.nav-links a');
     navLinkElements.forEach(link => {
-        link.addEventListener('click', () => {
-            const navLinks = document.getElementById('nav-links');
+        link.addEventListener('click', function() {
+            console.log('Nav link clicked');
+            const navLinks = document.querySelector('.nav-links');
             navLinks.classList.remove('active'); // Ensure the menu closes
         });
     });
