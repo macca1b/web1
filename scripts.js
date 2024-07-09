792eda6c7b5c9b02d628e4428228022c7271322c
macca1b <brendanmaccabe@gmail.com>
2024-07-09 01:10:26 +0100

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,5 +1,17 @@
-/* script.js */
-function toggleMenu() {
-    const navLinks = document.querySelector('.nav-links');
-    navLinks.classList.toggle('active');
-}
+document.addEventListener('DOMContentLoaded', function() {
+    function toggleMenu() {
+        const navLinks = document.getElementById('nav-links');
+        navLinks.classList.toggle('active');
+    }
+
+    const menuIcon = document.querySelector('.menu-icon');
+    menuIcon.addEventListener('click', toggleMenu);
+
+    const navLinkElements = document.querySelectorAll('.nav-links a');
+    navLinkElements.forEach(link => {
+        link.addEventListener('click', () => {
+            const navLinks = document.getElementById('nav-links');
+            navLinks.classList.remove('active'); // Ensure the menu closes
+        });
+    });
+});
